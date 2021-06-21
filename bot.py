368691b5b1ec10f0fb3ccee2073d632afd7d47bc
Felipe André <felipeeeep2003@gmail.com>
2021-06-21 16:35:20 -0300

    terminei

--- bot.py
+++ bot.py
@@ -3,11 +3,12 @@ from pywinauto import keyboard
 from getpass import getuser
 from time import sleep
 
+import os
+
 def acessTelegram():
     try:
 
         app = Application(backend='uia').start(f"C:\\Users\\{getuser()}\\AppData\\Roaming\\Telegram Desktop\\Telegram.exe")
-        
         sleep(3)
 
         app = Application(backend='uia').connect(title_re='Telegram')
@@ -16,13 +17,25 @@ def acessTelegram():
         keyboard.send_keys('Mensagens')
         sleep(2)
         keyboard.send_keys("{ENTER}")
+        sleep(3)
+
+        app.TelegramDesktop.GroupBox.GroupBox22.click_input(button='right', coords=(100, -100))
+        sleep(3)
+
+        app.TelegramDesktop.GroupBox.GroupBox3.click_input()
+        sleep(3)
+
+        inputlocal = app.TelegramDesktop.child_window(title="Todos os locais", control_type="SplitButton")
+        inputlocal.click_input()
         sleep(2)
 
-        app.TelegramDesktop.GroupBox.GroupBox12.click_input()
+        keyboard.send_keys(os.path.abspath('./'), with_spaces=True)
+        sleep(1)
+
+        keyboard.send_keys("{ENTER}")
         sleep(2)
-        
-        app.TelegramDesktop.GroupBox11.GroupBox2.GroupBox8.click_input()
-        
+
+        app.TelegramDesktop.Salvar.click_input()
         sleep(5)
 
         app.kill()
