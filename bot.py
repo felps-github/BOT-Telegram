0450c29669e77d99a778cbcdfe67611f78ff1a00
Felipe André <felipeeeep2003@gmail.com>
2021-06-21 14:48:37 -0300

    aaa

--- bot.py
+++ bot.py
@@ -5,23 +5,7 @@ from time import sleep
 
 def acessTelegram():
     try:
-        app = Application(backend='uia').start(f"C:\\Users\\{getuser()}\\AppData\\Roaming\\Telegram Desktop\\Telegram.exe")
-        sleep(3)
-        app = Application(backend='uia').connect(title_re='Telegram')
-
-        app.TelegramDesktop.set_focus()
-
-        keyboard.send_keys('Mensagens')
-        sleep(2)
-
-        keyboard.send_keys("{ENTER}")
-        sleep(2)
-
-        app.TelegramDesktop.GroupBox.GroupBox22.click_input(coords=(30, 60))
         
-        sleep(5)
-
-        app.kill()
 
     except Exception as error:
         print(error)
