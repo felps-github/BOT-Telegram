e7363e246ea90bc28e47241b0f1fba2630b00792
Felipe André <felipeeeep2003@gmail.com>
2021-06-22 17:51:16 -0300

    Bot finalizado
    Bot fazendo exatamente o que Dr Jumario queria e a imagem é apagada automaticamente após a mensagem enviada

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@ from pywinauto.application import Application
 from pywinauto import keyboard
 from getpass import getuser
 from time import sleep
+from PIL import Image
 
+import pytesseract
 import os
 
 def execute_bot():
@@ -16,33 +18,35 @@ def execute_bot():
 
         app.TelegramDesktop.set_focus()
         
-        keyboard.send_keys('Mensagens')
-        sleep(2)
+        keyboard.send_keys('Mensagens', pause=0)
+        sleep(1)
         keyboard.send_keys("{ENTER}")
         print("Contato selecionado")
 
-        sleep(3)
+        sleep(2)
 
     except Exception as error:
         print(error)
     
     save_photo()
+    ler_imagens()
+    write_text()
     sleep(3)
     app.kill()
 
 def save_photo():
     try:
         app.TelegramDesktop.GroupBox.GroupBox22.click_input(button='right', coords=(100, -100))
-        sleep(3)
+        sleep(2)
 
         app.TelegramDesktop.GroupBox.GroupBox3.click_input()
-        sleep(3)
+        sleep(2)
 
         inputlocal = app.TelegramDesktop.child_window(title="Todos os locais", control_type="SplitButton")
         inputlocal.click_input()
         sleep(1)
 
-        keyboard.send_keys(os.path.abspath('./'), with_spaces=True)
+        keyboard.send_keys(os.path.abspath('./'), with_spaces=True, pause=0)
         sleep(1)
 
         keyboard.send_keys("{ENTER}")
@@ -51,9 +55,31 @@ def save_photo():
         app.TelegramDesktop.Salvar.click_input()
 
         print("Imagem salva")
-        sleep(3)
+
+    except Exception as error:
+        print(error)     
+
+def write_text():
+    try:
+        keyboard.send_keys(pytesseract.image_to_string(Image.open(list_photos[-1]), config='--psm 6'), pause=0)
+        sleep(1)
+        keyboard.send_keys("{ENTER}")
+        
+        print("Texto enviado com sucesso")
+        os.remove(list_photos[-1])
 
     except Exception as error:
         print(error)
+
+def ler_imagens():
+    global list_photos, file
+    sleep(3)
+    path = os.path.abspath('./')
+    dirs = os.listdir(path)
+    list_photos = []
+
+    for file in dirs:
+        if ".jpg" in file or ".png" in file:
+            list_photos.append(file)
 
 execute_bot()
