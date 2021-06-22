1ab2cf0797846864987dbdc744f101aa31f41211
Felipe André <felipeeeep2003@gmail.com>
2021-06-22 15:10:06 -0300

    Baixando as imagens
    Porém, baixa apenas uma imagem por vez.

--- bot.py
+++ bot.py
@@ -5,20 +5,33 @@ from time import sleep
 
 import os
 
-def acessTelegram():
+def execute_bot():
     try:
-
+        global app
         app = Application(backend='uia').start(f"C:\\Users\\{getuser()}\\AppData\\Roaming\\Telegram Desktop\\Telegram.exe")
         sleep(3)
 
         app = Application(backend='uia').connect(title_re='Telegram')
+        print("Conectado ao Telegram")
+
         app.TelegramDesktop.set_focus()
         
         keyboard.send_keys('Mensagens')
         sleep(2)
         keyboard.send_keys("{ENTER}")
+        print("Contato selecionado")
+
         sleep(3)
 
+    except Exception as error:
+        print(error)
+    
+    save_photo()
+    sleep(3)
+    app.kill()
+
+def save_photo():
+    try:
         app.TelegramDesktop.GroupBox.GroupBox22.click_input(button='right', coords=(100, -100))
         sleep(3)
 
@@ -27,20 +40,20 @@ def acessTelegram():
 
         inputlocal = app.TelegramDesktop.child_window(title="Todos os locais", control_type="SplitButton")
         inputlocal.click_input()
-        sleep(2)
+        sleep(1)
 
         keyboard.send_keys(os.path.abspath('./'), with_spaces=True)
         sleep(1)
 
         keyboard.send_keys("{ENTER}")
-        sleep(2)
+        sleep(1)
 
         app.TelegramDesktop.Salvar.click_input()
-        sleep(5)
 
-        app.kill()
+        print("Imagem salva")
+        sleep(3)
 
     except Exception as error:
         print(error)
 
-acessTelegram()
+execute_bot()
